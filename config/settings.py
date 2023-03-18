b715e01105cb47f27f02a5f4e54a77c5a85c3f8b
Bao Tong <baotongxuan@gmail.com>
2023-03-18 20:34:29 +1100

    Moved sentry to outside

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,25 @@ import dj_database_url
 import os
 from django.test.runner import DiscoverRunner
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 
+sentry_sdk.init(
+    dsn="https://9aa9241803f442d19272107d10bce410@o4504830292000768.ingest.sentry.io/4504830295408640",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,26 +59,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 if not IS_HEROKU:
     DEBUG = True
-else:
-    import sentry_sdk
-    from sentry_sdk.integrations.django import DjangoIntegration
 
-
-    sentry_sdk.init(
-        dsn="https://9aa9241803f442d19272107d10bce410@o4504830292000768.ingest.sentry.io/4504830295408640",
-        integrations=[
-            DjangoIntegration(),
-        ],
-
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
-
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
-    )
 
 # Application definition
 
